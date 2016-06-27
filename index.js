ed78dd295ac883dadbffcbae5bf43933e9d31c35
Marc-Henri MALIGNAN <mhmalignan@chal.local>
2016-06-27 16:22:12 +0200

    config + bot model

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-var Discordie = require('discordie');
-var Events = Discordie.Events;
-var client = new Discordie();
-
-client.connect({ token: 'MTk2OTY4ODAzNDA4NTQzNzQ0.ClLArA.be5T8kevAyqv-es32PxjxTv5TjU' });
-
-client.Dispatcher.on(Events.GATEWAY_READY, e => {
-  console.log('Connected as: ' + client.User.username);
-});
-
-client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
-  if (e.message.content == 'ping') {
-    e.message.channel.sendMessage('pong');
-  }
-});
+var Bot = require('./lib/models/bot');
+var b = new Bot();
