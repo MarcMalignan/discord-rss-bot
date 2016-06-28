669c99e1186d68e062df5c4f85f3c44f4e9d9fba
Marc-Henri MALIGNAN <mhmalignan@chal.local>
2016-06-28 19:05:49 +0200

    index update

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
+var Discordie = require('discordie');
+var config = require('./lib/config');
 var Bot = require('./lib/models/bot');
-var b = new Bot();
+
+// App class
+function App() {
+
+  // get private params
+  var pvt;
+  try {
+    pvt = require('./lib/config-private');
+    for(var i in pvt) {
+      config[i] = pvt[i];
+    }
+  } catch(e) {
+    console.log('ERROR - ' + e);
+  }
+
+  // if got private params
+  if(pvt && pvt.appToken) {
+
+    // connect discord client
+    var client = new Discordie();
+    client.connect({ 
+      token: pvt.appToken,
+    });
+
+    // instantiate bot
+    var b = new Bot(client);
+  }
+};
+
+// launch app
+var app = new App();
